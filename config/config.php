6e19507106143270f784c7ef196e989d63bb54a3
Dilruba Khan <rubatanisa@gmail.com>
2018-07-04 01:10:36 +0100

    Create Database and Config class

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+//DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "form_post");
+
+define("SITE_TITLE", "Welcome To Form Post Application!");
+
+//Paths
+define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/form_post/');
