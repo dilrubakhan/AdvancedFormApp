6099825232dfdd28aa077e2cb7ea0a358fb1db2f
Dilruba Khan <rubatanisa@gmail.com>
2018-07-04 15:08:22 +0100

    Implement Template class and Include header and footer to the front page

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
 //DB Params
-define("DB_HOST", "localhost");
+define("DB_HOST", "localhost:8080");
 define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_PASS", " ");
 define("DB_NAME", "form_post");
 
 define("SITE_TITLE", "Welcome To Form Post Application!");
+
 
 //Paths
 define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/form_post/');
